baa8fae144e062e1c3e036f2769d354718172f60
j3ffhubb <j3ffhubb@noreply.github.com>
2017-12-31 09:19:42 -0800

    Add a module of auxiliary checks
    This is to prevent the type_assert* function signatures from
    getting too bloated.

---
+++ pymarshal/util/checks.py
@@ -0,0 +1,49 @@
+"""
+    Various utility functions to supplement the functionality of
+    the type assert functions
+"""
+
+
+__all__ = [
+    'check_dups',
+]
+
+
+def check_dups(
+    iterable,
+    debug_limit=1000,
+):
+    """ Checks an iterable for duplicates
+
+        Note that it does not make sense to call this on a set()
+
+        If calling on a collection without a .count method,
+        set @debug_limit to -1.
+
+        For custom equality comparisons, create a custom
+        __eq__ and __hash__ method in the class contained in @iterable
+
+    Args:
+        iterable:    An iterable to check for duplicates
+        debug_limit: Don't generate a list of duplicates for the
+                     Exception message if the length of @iterable
+                     is >= this value.  This exists because generating
+                     the list does not scale efficiently.
+    Raises:
+        ValueError: If @iterable contains duplicates
+    """
+    unique = set(iterable)
+    if len(unique) != len(iterable):
+        if len(iterable) < debug_limit:
+            dups = [x for x in unique if iterable.count(x) > 1]
+            msg = "Duplicate values: {}".format(dups)
+        else:
+            msg = (
+                "Duplicate values, not generating list because "
+                "len(iterable)={} is greater than debug_limit={}"
+            ).format(
+                len(iterable),
+                debug_limit,
+            )
+        raise ValueError(msg)
+
---
+++ test/pymarshal/util/test_checks.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+import collections
+
+import pytest
+
+from pymarshal.util.checks import *
+
+
+def test_check_dups():
+    # should not raise
+    check_dups([1, 2, 3])
+
+
+def test_check_dups_raises():
+    for iterable, debug_limit in (
+        ([1, 2, 3, 3], 1000),
+        ([1, 2, 2], 1),
+    ):
+        with pytest.raises(ValueError):
+            check_dups(iterable, debug_limit)
