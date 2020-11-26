3373c26da3465eb249a3eec317fa09f86539cfae
j3ffhubb <j3ffhubb@noreply.users.github.com>
2020-11-26 09:25:35 -0800

    Add an optional return value

--- pymarshal/util/pm_assert.py
+++ pymarshal/util/pm_assert.py
@@ -3,13 +3,16 @@ def pm_assert(
     exc=Exception,
     context=None,
     msg="",
+    ret=None,
 ):
     """ Generic assertion that can be used anywhere
         @condition: bool, A condition to assert is true
         @exc:       Exception type, Raise if @condition is False
         @context:   Any, The relevant data structures
         @msg:       str, Any additional text to include
+        @ret:       Any, Optional return value if @condition is True
     """
     if not condition:
         raise exc(msg + "\n" + str(context))
+    return ret
 
