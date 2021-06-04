0f8921565cb89977025fc6ddce8bffc5e2d69c25
stargateaudio <stargateaudio@noreply.github.com>
2021-06-03 18:57:28 -0700

    Remove return value API
    That was ill-conceived

--- pymarshal/util/pm_assert.py
+++ pymarshal/util/pm_assert.py
@@ -2,20 +2,18 @@ from typing import Any, Optional
 
 
 def pm_assert(
-    condition,
+    condition: Any,
     exc: Any=Exception,
     context: Any=None,
     msg: str="",
-    ret: Optional[Any]=None,
-) -> Optional[Any]:
+) -> Any:
     """ Generic assertion that can be used anywhere
         @condition: A condition to assert is true
         @exc:       Raise if @condition is False
         @context:   The relevant data structures
         @msg:       Any additional text to include
-        @ret:       return value if @condition is True
     """
     if not condition:
         raise exc(f"{msg}\n{context}")
-    return ret if ret is not None else condition
+    return condition
 
